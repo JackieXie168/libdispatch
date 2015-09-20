f613f98809332ea007737a7f409c8d1ac8f0d456
Nick Hutchinson <nick.hutchinson@thefoundry.co.uk>
2015-09-20 22:00:12 +0100

    Add failing test for issue #17

--- testing/test_dispatch.py
+++ testing/test_dispatch.py
@@ -5,6 +5,21 @@ import os
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.join(SCRIPT_DIR, '..')
 
+
+@pytest.mark.parametrize('cc', ['clang', 'gcc'])
+@pytest.mark.parametrize('filetype', ['c', 'c++'])
+def test_header_compat(cc, filetype):
+    if cc == 'gcc' and filetype == 'c++':
+        pytest.xfail("Issue #17")
+
+    ret = subprocess.call(
+        [
+            cc, '-x', filetype, '-I%s' % PROJECT_ROOT, '-fsyntax-only',
+            os.path.join(PROJECT_ROOT, 'dispatch/dispatch.h')
+        ])
+    assert ret == 0
+
+
 TESTS = [
     'after',
     'api',
